985780e45d968535d7fc37c9d7584dfdf7ed9510
Marc <marc@spittfire-local.home>
2020-03-03 00:56:51 +0100

    Added the Menu/MapUI

---
+++ app/src/androidTest/java/ch/epfl/sdp/kandle/MenuActivityTest.java
@@ -0,0 +1,4 @@
+package ch.epfl.sdp.kandle;
+
+public class MenuActivityTest {
+}
---
+++ app/src/main/java/ch/epfl/sdp/kandle/MenuActivity.java
@@ -0,0 +1,13 @@
+package ch.epfl.sdp.kandle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
