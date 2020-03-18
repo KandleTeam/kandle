c931e6b8f04e4b3aa94ec10c0ceafd8cac15a50b
nicojimestre <njimenez5007@gmail.com>
2020-03-18 20:01:52 +0100

    abstracted map class

---
+++ app/src/main/java/ch/epfl/sdp/kandle/AbstractLocation.java
@@ -0,0 +1,131 @@
+package ch.epfl.sdp.kandle;
+
+import android.Manifest;
+import android.content.Context;
+import android.location.Location;
+import android.os.Looper;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import permissions.dispatcher.NeedsPermission;
+
+import static com.google.android.gms.location.LocationServices.getFusedLocationProviderClient;
+
+public class AbstractLocation {
+
+    private Context context;
+    private long UPDATE_INTERVAL = 60000;  /* 60 secs */
+    private long FASTEST_INTERVAL = 5000; /* 5 secs */
+    private Location mCurrentLocation;
+    private GoogleMap mMap;
+    private LocationRequest mLocationRequest;
+
+    public AbstractLocation(Context context, Location mCurrentLocation){
+        this.context = context;
+        this.mCurrentLocation = mCurrentLocation;
+    }
+
+    public void setCurrentLocation(Location mCurrentLocation){
+        this.mCurrentLocation = mCurrentLocation;
+    }
+
+    public Location getCurrentLocation(){
+        return mCurrentLocation;
+    }
+
+    @NeedsPermission({android.Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION})
+    protected void startLocationUpdates() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(mLocationRequest);
+        LocationSettingsRequest locationSettingsRequest = builder.build();
+
+        SettingsClient settingsClient = LocationServices.getSettingsClient(context);
+        settingsClient.checkLocationSettings(locationSettingsRequest);
+        //noinspection MissingPermission
+        getFusedLocationProviderClient(context).requestLocationUpdates(mLocationRequest, new LocationCallback() {
+                    @Override
+                    public void onLocationResult(LocationResult locationResult) {
+                        mCurrentLocation = onLocationChanged(locationResult.getLastLocation());
+                    }
+                },
+                Looper.myLooper());
+    }
+
+
+
+    public Location onLocationChanged(Location location) {
+        // GPS may be turned off
+        if (location == null) {
+            return null;
+        }
+
+        // Report to the UI that the location was updated
+
+
+        String msg = "Updated Location: " +
+                Double.toString(location.getLatitude()) + "," +
+                Double.toString(location.getLongitude());
+        //Toast.makeText(this.getContext(), msg, Toast.LENGTH_SHORT).show();
+        return location;
+    }
+
+    protected void loadMap(GoogleMap googleMap) {
+        mMap = googleMap;
+        if (mMap != null) {
+            // Map is ready
+            Toast.makeText(context, "Map Fragment was loaded properly!", Toast.LENGTH_SHORT).show();
+            getMyLocation();
+            startLocationUpdates();
+
+        } else {
+            Toast.makeText(context, "Error - Map was null!!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @SuppressWarnings({"MissingPermission"})
+    @NeedsPermission({Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION})
+    void getMyLocation() {
+        mMap.setMyLocationEnabled(true);
+
+        FusedLocationProviderClient locationClient = getFusedLocationProviderClient(context);
+        locationClient.getLastLocation()
+                .addOnSuccessListener(new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 16));
+                        /*if (location != null) {
+                            onLocationChanged(location);
+                        }*/
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("MapDemoActivity", "Error trying to get last GPS location");
+                        e.printStackTrace();
+                    }
+                });
+    }
+
+}
--- app/src/main/java/ch/epfl/sdp/kandle/MapFragment.java
+++ app/src/main/java/ch/epfl/sdp/kandle/MapFragment.java
@@ -1,32 +1,19 @@
 package ch.epfl.sdp.kandle;
 
 
-import android.Manifest;
-import android.annotation.SuppressLint;
 import android.app.Dialog;
-import android.location.Location;
 import android.os.Bundle;
-import android.os.Looper;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -35,14 +22,8 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 
-
-import permissions.dispatcher.NeedsPermission;
 import permissions.dispatcher.RuntimePermissions;
-
-import static com.google.android.gms.location.LocationServices.getFusedLocationProviderClient;
 
 @RuntimePermissions
 public class MapFragment extends Fragment implements GoogleMap.OnMarkerClickListener {
@@ -50,10 +31,9 @@ public class MapFragment extends Fragment implements GoogleMap.OnMarkerClickList
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private GoogleMap mMap;
     private SupportMapFragment mapFragment;
-    private LocationRequest mLocationRequest;
-    Location mCurrentLocation;
-    private long UPDATE_INTERVAL = 60000;  /* 60 secs */
-    private long FASTEST_INTERVAL = 5000; /* 5 secs */
+    //Location mCurrentLocation;
+
+    AbstractLocation abstractLocation = new AbstractLocation(this.getContext(), null);
 
     private final static String KEY_LOCATION = "location";
     private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
@@ -71,6 +51,7 @@ public class MapFragment extends Fragment implements GoogleMap.OnMarkerClickList
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_map, container, false);
         mapFragment = (SupportMapFragment) this.getChildFragmentManager().findFragmentById(R.id.map);
@@ -78,7 +59,11 @@ public class MapFragment extends Fragment implements GoogleMap.OnMarkerClickList
             mapFragment.getMapAsync(new OnMapReadyCallback() {
                 @Override
                 public void onMapReady(GoogleMap map) {
-                    loadMap(map);
+                    abstractLocation.loadMap(map);
+                    map.addMarker(new MarkerOptions()
+                            .position(new LatLng(10, 10))
+                            .title("Hello world")).setTag(0);
+                    //map.setOnMarkerClickListener(this);
 
 
                 }
@@ -87,49 +72,6 @@ public class MapFragment extends Fragment implements GoogleMap.OnMarkerClickList
             Toast.makeText(this.getContext(), "Error - Map Fragment was null!!", Toast.LENGTH_SHORT).show();
         }
         return v;
-    }
-
-
-    protected void loadMap(GoogleMap googleMap) {
-        mMap = googleMap;
-        if (mMap != null) {
-            // Map is ready
-            Toast.makeText(this.getContext(), "Map Fragment was loaded properly!", Toast.LENGTH_SHORT).show();
-            getMyLocation();
-            startLocationUpdates();
-            mMap.addMarker(new MarkerOptions()
-                    .position(new LatLng(10, 10))
-                    .title("Hello world")).setTag(0);
-            mMap.setOnMarkerClickListener(this);
-        } else {
-            Toast.makeText(this.getContext(), "Error - Map was null!!", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-
-    @SuppressWarnings({"MissingPermission"})
-    @NeedsPermission({Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION})
-    void getMyLocation() {
-        mMap.setMyLocationEnabled(true);
-
-        FusedLocationProviderClient locationClient = getFusedLocationProviderClient(this.getContext());
-        locationClient.getLastLocation()
-                .addOnSuccessListener(new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
-                        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 16));
-                        if (location != null) {
-                            onLocationChanged(location);
-                        }
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.d("MapDemoActivity", "Error trying to get last GPS location");
-                        e.printStackTrace();
-                    }
-                });
     }
 
     /*
@@ -157,7 +99,7 @@ public class MapFragment extends Fragment implements GoogleMap.OnMarkerClickList
             Log.d("Location Updates", "Google Play services is available.");
             return true;
         } else {
-           return false;
+            return false;
 
         }
     }
@@ -168,58 +110,23 @@ public class MapFragment extends Fragment implements GoogleMap.OnMarkerClickList
 
         // Display the connection status
 
-        if (mCurrentLocation != null) {
+        if (abstractLocation.getCurrentLocation() != null) {
             Toast.makeText(this.getContext(), "GPS location was found!", Toast.LENGTH_SHORT).show();
-            LatLng latLng = new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
+            LatLng latLng = new LatLng(abstractLocation.getCurrentLocation().getLatitude(), abstractLocation.getCurrentLocation().getLongitude());
             CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 17);
             mMap.animateCamera(cameraUpdate);
         } else {
             Toast.makeText(this.getContext(), "Current location was null, enable GPS on emulator!", Toast.LENGTH_SHORT).show();
         }
-        startLocationUpdates();
+        abstractLocation.startLocationUpdates();
     }
 
-    @NeedsPermission({Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION})
-    protected void startLocationUpdates() {
-        mLocationRequest = new LocationRequest();
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
-        mLocationRequest.setInterval(UPDATE_INTERVAL);
-        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
-
-        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
-        builder.addLocationRequest(mLocationRequest);
-        LocationSettingsRequest locationSettingsRequest = builder.build();
-
-        SettingsClient settingsClient = LocationServices.getSettingsClient(this.getContext());
-        settingsClient.checkLocationSettings(locationSettingsRequest);
-        //noinspection MissingPermission
-        getFusedLocationProviderClient(this.getContext()).requestLocationUpdates(mLocationRequest, new LocationCallback() {
-                    @Override
-                    public void onLocationResult(LocationResult locationResult) {
-                        onLocationChanged(locationResult.getLastLocation());
-                    }
-                },
-                Looper.myLooper());
-    }
 
     //Cette fonction ne sert a rien elle affiche juste un toast que ne sert à rien dans le cadre de notre application
-    public void onLocationChanged(Location location) {
-        // GPS may be turned off
-        if (location == null) {
-            return;
-        }
 
-        // Report to the UI that the location was updated
-
-        mCurrentLocation = location;
-        String msg = "Updated Location: " +
-                Double.toString(location.getLatitude()) + "," +
-                Double.toString(location.getLongitude());
-        //Toast.makeText(this.getContext(), msg, Toast.LENGTH_SHORT).show();
-    }
 
     public void onSaveInstanceState(Bundle savedInstanceState) {
-        savedInstanceState.putParcelable(KEY_LOCATION, mCurrentLocation);
+        savedInstanceState.putParcelable(KEY_LOCATION, abstractLocation.getCurrentLocation());
         super.onSaveInstanceState(savedInstanceState);
     }
 
