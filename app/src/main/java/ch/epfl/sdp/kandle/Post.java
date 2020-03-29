f578097d8a22fe888cb0555c8a12e99b4ec300d2
Yanis Berkani <yanis.berkani@epfl.ch>
2020-03-29 14:47:27 +0200

    defensive programming for Lists of String and Date

--- app/src/main/java/ch/epfl/sdp/kandle/Post.java
+++ app/src/main/java/ch/epfl/sdp/kandle/Post.java
@@ -8,7 +8,9 @@ import com.google.android.gms.maps.model.LatLng;
 
 import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 
@@ -85,8 +87,8 @@ public class Post {
     }
      */
 
-    public ArrayList<String> getLikers(){
-        return new ArrayList<String>(likers);
+    public List<String> getLikers(){
+        return Collections.unmodifiableList(likers);
     }
 
     public int getLikes() {
@@ -116,7 +118,7 @@ public class Post {
      */
 
     public Date getDate() {
-        return date;
+        return (Date) date.clone();
     }
 
     public void likePost(String userId) {
