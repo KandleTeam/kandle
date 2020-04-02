6b8de3a45335883416855d75f7abaea7e9c8a070
Bolzano17 <anas.ibrahim@epfl.ch>
2020-04-02 16:52:38 +0200

    load profile fragment at first

--- app/src/main/java/ch/epfl/sdp/kandle/MainActivity.java
+++ app/src/main/java/ch/epfl/sdp/kandle/MainActivity.java
@@ -81,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
         drawerToggle.syncState();
         mDrawerLayout.addDrawerListener(drawerToggle);
 
-        createNewFragmentInstance(MapViewFragment.class);
+        createNewFragmentInstance(ProfileFragment.class);
         fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
         setTitle(mNavigationView.getCheckedItem().getTitle());
