96d6ff655213d698b5efa40a658ff1219b733267
Antoine Sidem <sidemantoine@gmail.com>
2020-06-03 10:34:03 +0200

    Refactor ProfileFragment

--- app/src/main/java/ch/epfl/sdp/kandle/fragment/ProfileFragment.java
+++ app/src/main/java/ch/epfl/sdp/kandle/fragment/ProfileFragment.java
@@ -27,32 +27,30 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import ch.epfl.sdp.kandle.R;
+import ch.epfl.sdp.kandle.dependencies.DependencyManager;
 import ch.epfl.sdp.kandle.entities.achievement.Achievement;
 import ch.epfl.sdp.kandle.entities.user.LoggedInUser;
-import ch.epfl.sdp.kandle.R;
 import ch.epfl.sdp.kandle.entities.user.User;
-import ch.epfl.sdp.kandle.authentification.Authentication;
 import ch.epfl.sdp.kandle.storage.Database;
-import ch.epfl.sdp.kandle.dependencies.DependencyManager;
-import ch.epfl.sdp.kandle.utils.imagePicker.ProfilePicPicker;
 import ch.epfl.sdp.kandle.storage.caching.CachedFirestoreDatabase;
+import ch.epfl.sdp.kandle.utils.imagePicker.ProfilePicPicker;
 
 
 public class ProfileFragment extends Fragment {
 
-    public final static int PROFILE_PICTURE_BEFORE = 6;
-    public final static int PROFILE_PICTURE_AFTER = 7;
+    private final static int PROFILE_PICTURE_BEFORE = 6;
+    private final static int PROFILE_PICTURE_AFTER = 7;
+    private static int nbValidatedAchievements;
     private User user;
     private ImageView mProfilePicture, mEditPicture, mProfilePictureInMenu, mEditName, mBadge;
     private TextView mNumberOfFollowers, mNumberOfFollowing, mUsername, mNicknameView, mNickNameInMenu;
     private EditText mNicknameEdit;
     private ViewSwitcher mNickname;
     private Button mFollowButton, mValidateNameButton, mValidatePictureButton;
-    private Authentication auth;
-    private Database database;
     private Uri imageUri;
     private List<Achievement> achievements;
-    private static int nbValidatedAchievements;
+    private Database database;
 
     private ProfileFragment(User user) {
         this.user = user;
@@ -84,23 +82,15 @@ public class ProfileFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        AchievementFragment.getAchievements(achievements);
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
-        auth = DependencyManager.getAuthSystem();
         database = new CachedFirestoreDatabase();
 
         getViews(view);
 
-        final User currentUser = LoggedInUser.getInstance();
-
-        mValidateNameButton.setVisibility(View.GONE);
-        mValidatePictureButton.setVisibility(View.GONE);
-
-        if (!user.getId().equals(currentUser.getId()) || LoggedInUser.isGuestMode()) {
+        if (!user.getId().equals(LoggedInUser.getInstance().getId()) || LoggedInUser.isGuestMode()) {
             mEditPicture.setVisibility(View.GONE);
             mEditName.setVisibility(View.GONE);
         } else {
@@ -108,24 +98,83 @@ public class ProfileFragment extends Fragment {
                 mEditPicture.setVisibility(View.GONE);
                 ProfilePicPicker.openImage(this);
             });
-
         }
-        nbValidatedAchievements = 0;
-        for(Achievement achievement: achievements){
 
+        AchievementFragment.getAchievements(achievements);
+        nbValidatedAchievements = 0;
+        for (Achievement achievement : achievements) {
             achievement.setProfileFragment(this);
             achievement.checkAchievement(false);
         }
+        changeBadge();
 
-       changeBadge();
+        mValidateNameButton.setVisibility(View.GONE);
+        mValidatePictureButton.setVisibility(View.GONE);
 
         mEditName.setOnClickListener(v -> {
             mEditName.setVisibility(View.GONE);
             mValidateNameButton.setVisibility(View.VISIBLE);
             mNickname.showNext();
         });
+        mUsername.setText(String.format("@%s", user.getUsername()));
 
-        mValidateNameButton.setOnClickListener(v -> {
+        mValidateNameButton.setOnClickListener(validateNameClickListener());
+        mValidatePictureButton.setOnClickListener(validatePictureClickListener());
+
+        mNicknameView = mNickname.findViewById(R.id.text_view);
+        mNicknameEdit = mNickname.findViewById(R.id.edit_view);
+        mNicknameView.setText(user.getNickname());
+        mNicknameEdit.setText(user.getNickname());
+
+        loadProfilePicture();
+
+        setNumberOfFollowers();
+        setNumberOfFollowing();
+
+        setupFollowButton(LoggedInUser.getInstance());
+
+        final FragmentManager fragmentManager = this.getActivity().getSupportFragmentManager();
+        mNumberOfFollowers.setOnClickListener(v -> database.userFollowersList(user.getId()).addOnCompleteListener(numberListener("Followers", fragmentManager)));
+        mNumberOfFollowing.setOnClickListener(v -> database.userFollowingList(user.getId()).addOnCompleteListener(numberListener("Following", fragmentManager)));
+        mNumberOfFollowers.setClickable(!LoggedInUser.isGuestMode());
+        mNumberOfFollowing.setClickable(!LoggedInUser.isGuestMode());
+
+
+        return view;
+    }
+
+    private void loadProfilePicture() {
+        if (user.getImageURL() != null) {
+            mProfilePicture.setTag(PROFILE_PICTURE_BEFORE);
+            File image = DependencyManager.getInternalStorageSystem().getImageFileById(user.getId());
+            if (image != null) {
+                Picasso.get().load(image).into(mProfilePicture);
+            } else {
+                Picasso.get().load(user.getImageURL()).into(mProfilePicture);
+            }
+        }
+    }
+
+    private void setupFollowButton(User currentUser) {
+
+        if (user.getId().equals(currentUser.getId()) || LoggedInUser.isGuestMode()) {
+            mFollowButton.setVisibility(View.GONE);
+        } else {
+            database.userIdFollowingList(LoggedInUser.getInstance().getId()).addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    if (task.getResult() == null || !task.getResult().contains(user.getId())) {
+                        mFollowButton.setText(R.string.followBtnNotFollowing);
+                    } else {
+                        mFollowButton.setText(R.string.followBtnAlreadyFollowing);
+                    }
+                }
+            });
+            mFollowButton.setOnClickListener(followButtonListener(currentUser));
+        }
+    }
+
+    private View.OnClickListener validateNameClickListener() {
+        return v -> {
 
             InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
             if (getActivity().getCurrentFocus() != null) {
@@ -137,15 +186,17 @@ public class ProfileFragment extends Fragment {
             if (nickname.trim().length() > 0) {
                 mNicknameView.setText(nickname.trim());
                 mNickNameInMenu.setText(nickname.trim());
-                auth.getCurrentUser().setNickname(nickname.trim());
+                DependencyManager.getAuthSystem().getCurrentUser().setNickname(nickname.trim());
                 database.updateNickname(nickname.trim());
             }
             mNickname.showPrevious();
             mValidateNameButton.setVisibility(View.GONE);
             mEditName.setVisibility(View.VISIBLE);
-        });
+        };
+    }
 
-        mValidatePictureButton.setOnClickListener(v -> {
+    private View.OnClickListener validatePictureClickListener() {
+        return v -> {
             ProgressDialog pd = new ProgressDialog(getContext());
             pd.setMessage("uploading");
             pd.show();
@@ -156,56 +207,7 @@ public class ProfileFragment extends Fragment {
             });
             mValidatePictureButton.setVisibility(View.GONE);
             mEditPicture.setVisibility(View.VISIBLE);
-        });
-
-        mNicknameView = mNickname.findViewById(R.id.text_view);
-        mNicknameEdit = mNickname.findViewById(R.id.edit_view);
-        mNicknameView.setText(user.getNickname());
-        mNicknameEdit.setText(user.getNickname());
-
-        mUsername.setText(String.format("@%s", user.getUsername()));
-        if (user.getImageURL() != null) {
-            mProfilePicture.setTag(PROFILE_PICTURE_BEFORE);
-            File image = DependencyManager.getInternalStorageSystem().getImageFileById(user.getId());
-            if(image != null) {
-                Picasso.get().load(image).into(mProfilePicture);
-                System.out.println("Fetched from internal storage");
-            }else {
-                Picasso.get().load(user.getImageURL()).into(mProfilePicture);
-            }
-        }
-
-        setNumberOfFollowers();
-        setNumberOfFollowing();
-
-
-        if (user.getId().equals(currentUser.getId())) {
-            mFollowButton.setVisibility(View.GONE);
-        } else {
-            database.userIdFollowingList(currentUser.getId()).addOnCompleteListener(task -> {
-                if (task.isSuccessful()) {
-                    if ((task.getResult() == null) || (!task.getResult().contains(user.getId()))) {
-                        mFollowButton.setText(R.string.followBtnNotFollowing);
-                    } else {
-                        mFollowButton.setText(R.string.followBtnAlreadyFollowing);
-                    }
-                }
-            });
-            mFollowButton.setOnClickListener(followButtonListener(currentUser));
-        }
-
-        final FragmentManager fragmentManager = this.getActivity().getSupportFragmentManager();
-
-
-        mNumberOfFollowers.setOnClickListener(v -> database.userFollowersList(user.getId()).addOnCompleteListener(numberListener("Followers", fragmentManager)));
-        mNumberOfFollowing.setOnClickListener(v -> database.userFollowingList(user.getId()).addOnCompleteListener(numberListener("Following", fragmentManager)));
-        if (LoggedInUser.isGuestMode()) {
-            mNumberOfFollowers.setClickable(false);
-            mNumberOfFollowing.setClickable(false);
-        }
-
-
-        return view;
+        };
     }
 
     private OnCompleteListener<List<User>> numberListener(String title, final FragmentManager fragmentManager) {
@@ -220,7 +222,6 @@ public class ProfileFragment extends Fragment {
                         .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
                         .addToBackStack(null)
                         .commit();
-
             }
         };
     }
@@ -266,7 +267,6 @@ public class ProfileFragment extends Fragment {
         });
     }
 
-
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -279,27 +279,25 @@ public class ProfileFragment extends Fragment {
         }
     }
 
-    public void notifyChange(){
+    public void notifyChange() {
         nbValidatedAchievements++;
         changeBadge();
     }
 
-    private void changeBadge(){
+    private void changeBadge() {
         mBadge.setTag(R.drawable.ic_icons2_medal_64);
-        if(nbValidatedAchievements > 0 && nbValidatedAchievements <= 2){
+
+        if (nbValidatedAchievements > 0 && nbValidatedAchievements <= 2) {
             mBadge.setImageResource(R.drawable.ic_icons2_medal_64);
             mBadge.setTag(R.drawable.ic_icons2_medal_64);
-        }
-        else if (nbValidatedAchievements <= 4 && nbValidatedAchievements > 2){
+
+        } else if (nbValidatedAchievements <= 4 && nbValidatedAchievements > 2) {
             mBadge.setImageResource(R.drawable.ic_icons1_medal_64);
             mBadge.setTag(R.drawable.ic_icons1_medal_64);
-
-        }
-        else if (nbValidatedAchievements <= 6 && nbValidatedAchievements > 4){
+        } else if (nbValidatedAchievements <= 6 && nbValidatedAchievements > 4) {
             mBadge.setImageResource(R.drawable.icons8_medal_64_1);
             mBadge.setTag(R.drawable.icons8_medal_64_1);
-        }
-        else if(nbValidatedAchievements > 6){
+        } else if (nbValidatedAchievements > 6) {
             mBadge.setImageResource(R.drawable.icons8_medal_64_2);
             mBadge.setTag(R.drawable.icons8_medal_64_2);
         }
